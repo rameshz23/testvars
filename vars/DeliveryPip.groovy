cf8973aedf0cc4ad17d9624b735ec12361c4bb6c
rameshz23 <rameshz23@yahoo.com>
2020-04-01 19:18:46 +0530

    Create DeliveryPip.groovy

---
+++ vars/DeliveryPip.groovy
@@ -0,0 +1,24 @@
+def call(Map pipelineParams) {
+
+    pipeline {
+        agent any
+        discardOldItems {
+            numToKeep(5)
+        }
+        stages {
+            stage('checkout git') {
+                steps {
+                    script{
+                    echo hello
+                    }
+                }
+            }
+
+            stage('build') {
+                steps {
+                    script{
+                    echo hello
+                    }
+                }
+            }
+       }
