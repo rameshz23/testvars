0feb3d3ac5dc9c23e59e5e8ba1bb8aa8003e3b19
rameshz23 <rameshz23@yahoo.com>
2020-04-02 15:38:56 +0530

    Update DeliveryPip.groovy

--- vars/DeliveryPip.groovy
+++ vars/DeliveryPip.groovy
@@ -1,49 +1,17 @@
 def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
     pipeline {
         options {
                 buildDiscarder(logRotator(numToKeepStr: '10', artifactNumToKeepStr: '10'))
                 }
-  		agent any
-		stages{
-              stage('Prepare environment'){
-                  steps{
-          		COLOR = "green"
-        echo "color is $COLOR"
-
-        sh '''COLOR=${COLOR}
-        echo $COLOR'''
-              }
-	stage('Delete'){
-         COLOR = "green"
-        echo "color is $COLOR"
-
-        sh '''COLOR=${COLOR}
-        echo $COLOR'''
-         }
-        stage('Create'){
-         COLOR = "green"
-        echo "color is $COLOR"
-
-        sh '''COLOR=${COLOR}
-        echo $COLOR'''
-	}
         
-	stage('Deploy'){
-         COLOR = "green"
-        echo "color is $COLOR"
-
-        sh '''COLOR=${COLOR}
-        echo $COLOR'''
-         }
-        
-	stage('Status Updater'){
-        COLOR = "green"
-        echo "color is $COLOR"
-
-        sh '''COLOR=${COLOR}
-        echo $COLOR'''
-         }
-}        
- }
- }
+        agent any
+        stage('Execute shell') {
+			var = "name"
+			sh "echo Hello ${var}"
+			}
+    }
 }
