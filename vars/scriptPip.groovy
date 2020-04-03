60b61dfb95bac14b7c218eb906c57063ac898989
rameshz23 <rameshz23@yahoo.com>
2020-04-03 19:49:23 +0530

    Update scriptPip.groovy

--- vars/scriptPip.groovy
+++ vars/scriptPip.groovy
@@ -29,11 +29,12 @@ def call(body) {
 		    }
 	    }
     }
-	    post {
-		    sucess {
-			    sparkSend credentialsId: 'webex', message: '${JOB_NAME} - ${BUILD_RESULT} - ${JOB_URL}', messageType: 'text', spaceList: [[spaceId: '768a8310-7348-11ea-8dca-b5cc1c3a792c', spaceName: 'common']]
-		    }
-	   }
-    
-    }   }
+  post {
+    success {
+	    //sparkSend credentialsId: 'webex', message: '${JOB_NAME} - ${BUILD_RESULT} - ${JOB_URL}', messageType: 'text', spaceList: [[spaceId: '768a8310-7348-11ea-8dca-b5cc1c3a792c', spaceName: 'common']]
+	    sparkSend credentialsId: 'webex', disable: true, failOnError: true, message: '${JOB_NAME} - ${BUILD_RESULT} - ${JOB_URL}', messageType: 'text', spaceList: [[spaceId: '768a8310-7348-11ea-8dca-b5cc1c3a792c', spaceName: 'common']]
+     	}
+   }    
+  }
+}
    
