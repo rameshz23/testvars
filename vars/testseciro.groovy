524a9470dca9dfb28814da258d3a9c32147fa122
rameshz23 <rameshz23@yahoo.com>
2020-04-10 13:33:49 +0530

    Update testseciro.groovy

--- vars/testseciro.groovy
+++ vars/testseciro.groovy
@@ -1,1 +1,30 @@
 
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    pipeline {
+            options {
+                buildDiscarder(logRotator(numToKeepStr: '5', artifactNumToKeepStr: '5'))
+                }
+        agent any
+        stages {
+            stage('This one should be skipped') {
+                when {
+                    expression { false }
+                }
+                steps {
+                    echo "this should be skipped, but it does not ("
+                }
+            }
+          stage('call'){
+            script{
+              echo "Welcome to script file"
+            }
+          }
+          
+        }//eod stages
+    } //eod pip
+}//eod def
